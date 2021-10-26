fe87e70d8f90cb754dde58a6766586f46be3412d
DESKTOP-I3VV3S0\Development <cjcarpenter1989@gmail.com>
2021-10-26 11:03:34 -0700

    Tied in core web api methods

--- react-game-app/src/App.js
+++ react-game-app/src/App.js
@@ -3,42 +3,94 @@ import './App.css';
 import Header from './components/Header'
 import SandboxNav from './components/SandboxNav'
 import SandboxArea from './components/SandboxArea'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [sandboxElements, setSandboxElements] = useState([
-    {
-      key: 1,
-      style: {
-        color: "red"
-      }
-    },
-    {
-      key: 2,
-      style: {
-        color: "blue"
-      }
+  // const [sandboxElements, setSandboxElements] = useState([
+  //   {
+  //     key: 1,
+  //     style: {
+  //       color: "red"
+  //     }
+  //   },
+  //   {
+  //     key: 2,
+  //     style: {
+  //       color: "blue"
+  //     }
+  //   }
+  // ]);
+
+  const [sandboxElements, setSandboxElements] = useState([]);
+
+  useEffect(() => {
+    const getSandboxElements = async () => {
+      const sandboxElements = await fetchSandboxElements();
+      setSandboxElements(sandboxElements);
     }
-  ]);
+
+    getSandboxElements();
+  }, [])
+
+  const fetchSandboxElements = async () => {
+    const response = await fetch('https://localhost:5001/api/ReactSandbox/GetAllSandboxElements').then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      return response.json();
+    }).catch(err => {
+      console.log(err)
+    })
+
+    const data = await response;
+    return data;
+  }
 
   // Create Element
-  const createElement = ({ id = null, color = null }) => {
+  const createElement = async ({ id = null, color = null }) => {
     const element = {
-      key: id ?? Math.floor(Math.random() * 10000),
+      //key: id ?? Math.floor(Math.random() * 10000),
       style: {
         color: color ?? ("#" + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'))
       }
     }
 
-    setSandboxElements([...sandboxElements, element]);
+    const response = await fetch(`https://localhost:5001/api/ReactSandbox/CreateSandboxElement`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(element)
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        
+        return response.json();
+      }).catch(err => {
+        console.log(err)
+      });
+
+    const data = await response
+    setSandboxElements([...sandboxElements, data]);
   }
+
 
   const addSandboxElement = ({ id, color }) => {
     createElement({ id, color });
   }
 
   // Delete Element
-  const deleteElement = (key) => {
+  const deleteElement = async (key) => {
+    await fetch(`https://localhost:5001/api/ReactSandbox/DeleteSandboxElement/${key}`, { method: 'DELETE' }).then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+    }).catch(err => {
+      console.log(err)
+    });
+
     setSandboxElements(sandboxElements.filter((sandboxElement) => sandboxElement.key !== key))
   }
 
